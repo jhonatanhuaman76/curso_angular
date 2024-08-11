30b3259c0b85e506d880bd1e08041fec02ddff1a
Jhonatan <jhonatanhuaman76@gmail.com>
2024-08-10 20:29:40 -0500

    class binding

---
+++ src/app/botones/botones.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-botones',
+  templateUrl: './botones.component.html',
+  styleUrl: './botones.component.css'
+})
+export class BotonesComponent {
+  css_text_color: string = '';
+  css_button_disabled: boolean = false;
+}
