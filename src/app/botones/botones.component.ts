ff5f02fd952bb77e63279d2c000de1a52cad20bc
Jhonatan <jhonatanhuaman76@gmail.com>
2024-08-10 20:35:10 -0500

    Property Binding

--- src/app/botones/botones.component.ts
+++ src/app/botones/botones.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class BotonesComponent {
   css_text_color: string = '';
   css_button_disabled: boolean = false;
+  src: string = 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg';
 }
